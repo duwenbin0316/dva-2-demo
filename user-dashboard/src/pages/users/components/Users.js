1b1f3bbfb8a9fb7a73381e1c5ff7b6189a3ffef0
duwenbin0316 <duwenbin0316@163.com>
2018-04-15 14:31:12 +0800

    CRUD done

--- user-dashboard/src/pages/users/components/Users.js
+++ user-dashboard/src/pages/users/components/Users.js
@@ -1,11 +1,37 @@
 import { connect } from 'dva';
-import { Table, Pagination, Popconfirm } from 'antd';
+import { Table, Pagination, Popconfirm, Button } from 'antd';
+import { routerRedux } from 'dva/router';
 import styles from './Users.css';
 import { PAGE_SIZE } from '../constants';
+import UserModal from './UserModal';
 
-function Users({ list: dataSource, total, page: current }) {
+function Users({ dispatch, list: dataSource, loading, total, page: current }) {
   function deleteHandler(id) {
-    console.warn(`TODO: ${id}`);
+    dispatch({
+      type: 'users/remove',
+      payload: id
+    })
+  }
+
+  function handleChange(page) {
+    dispatch(routerRedux.push({
+      pathname: '/users',
+      query: { page },
+    }));
+  }
+
+  function editHandler(id, values) {
+    dispatch({
+      type: 'users/patch',
+      payload: { id, values },
+    });
+  }
+
+  function createHandler(values){
+    dispatch({
+      type: 'users/create',
+      payload: values,
+    });
   }
 
   const columns = [
@@ -28,10 +54,12 @@ function Users({ list: dataSource, total, page: current }) {
     {
       title: 'Operation',
       key: 'operation',
-      render: (text, { id }) => (
+      render: (text, record) => (
         <span className={styles.operation}>
-          <a href="">Edit</a>
-          <Popconfirm title="Confirm to delete?" onConfirm={deleteHandler.bind(null, id)}>
+          <UserModal record={record} onOk={editHandler.bind(null, record.id)}>
+            <a >Edit</a>
+          </UserModal>
+          <Popconfirm title="Confirm to delete?" onConfirm={deleteHandler.bind(null, record.id)}>
             <a href="">Delete</a>
           </Popconfirm>
         </span>
@@ -41,10 +69,14 @@ function Users({ list: dataSource, total, page: current }) {
 
   return (
     <div className={styles.normal}>
+      <UserModal record={{}} onOk={createHandler.bind(this)}>
+        <Button className={ styles.create }>Create User</Button>
+      </UserModal>
       <div>
         <Table
           columns={columns}
           dataSource={dataSource}
+          loading={loading}
           rowKey={record => record.id}
           pagination={false}
         />
@@ -53,6 +85,7 @@ function Users({ list: dataSource, total, page: current }) {
           total={total}
           current={current}
           pageSize={PAGE_SIZE}
+          onChange={handleChange}
         />
       </div>
     </div>
@@ -65,6 +98,7 @@ function mapStateToProps(state) {
     list,
     total,
     page,
+    loading: state.loading.models.users,
   };
 }
 
